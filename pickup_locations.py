8d72d4a4c9816a6aef10b14932c8fee6fc06c180
slysylvia <sylviaxh@gmail.com>
2017-08-12 22:01:31 -0700

    working on grouping addresses

--- pickup_locations.py
+++ pickup_locations.py
@@ -16,4 +16,54 @@ def get_address_list():
     
     return addresses
 
-address_list = get_address_list()
+
+addresses = get_address_list()
+#print(*addresses, sep='\n')
+
+
+cities = ['Beverly Hills', 'Century City', 'Hollywood', 'LAX', 'Marina Del Rey', 'North Hollywood', 'Santa Monica', 'Venice', 'West Hollywood', 'Westwood']
+
+def group_pickup_locations(addresses):
+    
+    location_groups = dict.fromkeys(cities, [])
+    
+    for address in addresses:
+        if address in cities:
+            location_groups[city].append(address)
+    
+    return location_groups
+
+location_groups = group_pickup_locations(addresses)
+print(location_groups)
+
+
+cities = ['Beverly Hills', 'Westwood']
+location_groups = dict.fromkeys(cities, [])
+for city in cities:
+    print('CITY: ', city)
+    for address in addresses:
+        print('ADDRESS: ', address)
+        if city in address:
+            print('HERE', city)
+            location_groups[city].append(address)
+        else:
+            print('FALSE')
+            
+            
+cities = ['Beverly Hills', 'Westwood']
+location_groups = dict.fromkeys(cities, [])       
+for address in addresses:
+    print('ADDRESS: ', address)
+    for city in cities:
+        print('CITY: ', city)
+        if city in address:
+            print('HERE', city)
+            location_groups[city].append(address)
+            print('END')
+        else:
+            print('NOPE')
+
+
+
+#    if any(city in address for city in cities):
+
